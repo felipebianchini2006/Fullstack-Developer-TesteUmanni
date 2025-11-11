020769f7a55e155ffecb427937f2aaefa48a7a4c
felipebianchini2006 <kickbuttwovisk@gmail.com>
2025-11-10 22:39:22 -0300

    feat: Integrate Action Cable for real-time features; configure Sidekiq for background jobs; update importmap to include Action Cable

--- umanni_user_management/app/javascript/application.js
+++ umanni_user_management/app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import * as ActionCable from "@rails/actioncable"
+
+// Create global Action Cable consumer
+window.Cable = ActionCable.createConsumer()
--- umanni_user_management/config/environments/development.rb
+++ umanni_user_management/config/environments/development.rb
@@ -28,6 +28,9 @@ Rails.application.configure do
   # Change to :null_store to avoid any caching.
   config.cache_store = :memory_store
 
+  # Use Sidekiq for background jobs
+  config.active_job.queue_adapter = :sidekiq
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
